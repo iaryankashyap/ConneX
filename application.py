2cdcc2f29e79b6be104d60385108c7413eecf703
Aryan Kashyap <aryank.kashyap77783@gmail.com>
2020-05-26 12:17:04 +0530

    Added Full-featured signup-login

---
+++ application.py
@@ -0,0 +1,33 @@
+from cs50 import SQL
+from flask import Flask, flash, request, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def default():
+    return render_template("mainpage.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/login-dark")
+def darklogin():
+    return render_template("login-dark.html")
+
+
+@app.route("/signup-dark")
+def darksignup():
+    return render_template("signup-dark.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
