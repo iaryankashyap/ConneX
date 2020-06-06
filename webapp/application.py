e3db7641083a37f06f7f13aab9fb0230862c6f65
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-06-06 15:34:37 +0530

    Added Signup page

--- webapp/application.py
+++ webapp/application.py
@@ -2,11 +2,18 @@ from flask import Flask, flash, request, redirect, render_template
 
 app = Flask(__name__)
 
+loginpage = "login.html"
+signuppage = "signup.html"
+
 
 @app.route("/")
 def login():
-    return render_template("main.html")
+    return render_template(loginpage)
 
+
+@app.route("/signup")
+def signup():
+    return render_template(signuppage)
 
 '''
 if __name__ == "__main__":
