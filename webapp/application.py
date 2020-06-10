537103b2016d2933332d6ca32f50f8ccd4d497c5
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-06-10 21:36:15 +0530

    Added Admin Panel

--- webapp/application.py
+++ webapp/application.py
@@ -16,7 +16,10 @@ def getusers():
     sqcon = sq.connect(host='connex.mysql.pythonanywhere-services.com', database='connex$users',
                        user='connex', password='rootrootroot')
     cursor = sqcon.cursor()
+    users = []
     cursor.execute("SELECT * FROM users")
+    for i in cursor:
+        users.append(i[0])
     users = cursor
     sqcon.close()
     return users
