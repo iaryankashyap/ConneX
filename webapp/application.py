f1b0b11c694d3feffe6ce137cccf6a42b2d365c4
iaryankashyap <aryank.kashyap77783@gmail.com>
2021-08-19 00:27:43 +0530

    Med1Stop

--- webapp/application.py
+++ webapp/application.py
@@ -119,7 +119,8 @@ def getcon():
 
 @app.route("/")
 def land():
-    return render_template("landing.html")
+    #return render_template("landing.html")
+    return redirect("/login")
 
 
 @app.route("/login")
@@ -138,20 +139,20 @@ def login_details():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if username == "iaryankashyap" and password == "superuser2022":
-            logged = True
-            users = getusers()
-            usercount = usercoun()
-            emails = getemails()
-            data = getcon()
-            return render_template("admin.html", users=users, usercount=usercount, data=data, emails=emails, nm="Aryan")
-        if username == "LMxD" and password == "lmxd12345":
-            logged = True
-            users = getusers()
-            usercount = usercoun()
-            emails = getemails()
-            data = getcon()
-            return render_template("admin.html", users=users, usercount=usercount, data=data, emails=emails, nm="Lakshay")
+        # if username == "iaryankashyap" and password == "superuser2022":
+        #     logged = True
+        #     users = getusers()
+        #     usercount = usercoun()
+        #     emails = getemails()
+        #     data = getcon()
+        #     return render_template("admin.html", users=users, usercount=usercount, data=data, emails=emails, nm="Aryan")
+        # if username == "LMxD" and password == "lmxd12345":
+        #     logged = True
+        #     users = getusers()
+        #     usercount = usercoun()
+        #     emails = getemails()
+        #     data = getcon()
+        #     return render_template("admin.html", users=users, usercount=usercount, data=data, emails=emails, nm="Lakshay")
         if log_check(username, password):
             logged = True
             return redirect("/homepage_connex")
@@ -159,9 +160,9 @@ def login_details():
             return render_template("loginfail.html")
 
 
-@app.route("/connex_contact")
-def concontct():
-    return render_template("connexcontact.html")
+# @app.route("/connex_contact")
+# def concontct():
+#     return render_template("connexcontact.html")
 
 
 @app.route("/forgot_connex")
@@ -216,9 +217,9 @@ def conhome():
         return render_template("newlogin.html", error="Please login to continue.")
 
 
-@app.route("/admin_home")
-def adhome():
-    return render_template("homepage2.html")
+# @app.route("/admin_home")
+# def adhome():
+#     return render_template("homepage2.html")
 
 
 @app.route("/otp", methods=["GET", "POST"])
@@ -251,205 +252,205 @@ def logout():
     logged = False
     return redirect("/")
 
-# READX
+# # READX
 
 
-gr = 0
-home_main = "homev7.html"
-home_dark = "home-darkv7.html"
-grade_main = "gradev7.html"
-grade_main_dark = "grade-darkv7.html"
-contact_us = "contactv7.html"
-contact_us_dark = "contact-darkv7.html"
-fin = "finv7.html"
-findr = "findarkv7.html"
+# gr = 0
+# home_main = "homev7.html"
+# home_dark = "home-darkv7.html"
+# grade_main = "gradev7.html"
+# grade_main_dark = "grade-darkv7.html"
+# contact_us = "contactv7.html"
+# contact_us_dark = "contact-darkv7.html"
+# fin = "finv7.html"
+# findr = "findarkv7.html"
 
 
-def grade(text):
-    letters = 0
-    words = 0
-    sentences = 0
-    counter = 0
+# def grade(text):
+#     letters = 0
+#     words = 0
+#     sentences = 0
+#     counter = 0
 
-    for i in text:
-        counter += 1
+#     for i in text:
+#         counter += 1
 
-    for i in range(counter):
-        # counts the letters using ascii code
-        if (ord(text[i]) >= 65 and ord(text[i]) <= 122):
-            letters += 1
+#     for i in range(counter):
+#         # counts the letters using ascii code
+#         if (ord(text[i]) >= 65 and ord(text[i]) <= 122):
+#             letters += 1
 
-        # counts the words by reading spaces
-        elif (ord(text[i]) == 32 and (ord(text[i - 1]) != 33 and ord(text[i - 1]) != 46 and ord(text[i - 1]) != 63)):
-            words += 1
+#         # counts the words by reading spaces
+#         elif (ord(text[i]) == 32 and (ord(text[i - 1]) != 33 and ord(text[i - 1]) != 46 and ord(text[i - 1]) != 63)):
+#             words += 1
 
-        # counts the sentences by finding dots, exclamation marks and interrogatives
-        elif (ord(text[i]) == 33 or ord(text[i]) == 46 or ord(text[i]) == 63):
-            sentences += 1
-            words += 1
-    if words == 0:
-        words = 1
+#         # counts the sentences by finding dots, exclamation marks and interrogatives
+#         elif (ord(text[i]) == 33 or ord(text[i]) == 46 or ord(text[i]) == 63):
+#             sentences += 1
+#             words += 1
+#     if words == 0:
+#         words = 1
 
-    L = letters * 100 / words
-    S = sentences * 100 / words
-    # Coleman-Liau index is computed using the formula
-    index = round(0.0588 * L - 0.296 * S - 15.8)
+#     L = letters * 100 / words
+#     S = sentences * 100 / words
+#     # Coleman-Liau index is computed using the formula
+#     index = round(0.0588 * L - 0.296 * S - 15.8)
 
-    # Finally outputs the result to the user
-    if (index < 1):
-        return "Before 1"
+#     # Finally outputs the result to the user
+#     if (index < 1):
+#         return "Before 1"
 
-    elif (index >= 16):
-        return "16+"
+#     elif (index >= 16):
+#         return "16+"
 
-    else:
-        return index
-
-
-@app.route("/readx")
-def home():
-    if logged == True:
-        return render_template(home_main)
-    else:
-        return render_template("newlogin.html", error="Please login to continue.")
+#     else:
+#         return index
 
 
-@app.route("/home-dark")
-def homedark():
-    return render_template(home_dark)
+# @app.route("/readx")
+# def home():
+#     if logged == True:
+#         return render_template(home_main)
+#     else:
+#         return render_template("newlogin.html", error="Please login to continue.")
 
 
-@app.route("/para_details", methods=["GET", "POST"])
-def detpara():
-    if request.method == "POST":
-        var = request.form.get("subject")
-        gr = grade(var)
-        return render_template(grade_main, grade=gr)
+# @app.route("/home-dark")
+# def homedark():
+#     return render_template(home_dark)
 
 
-@app.route("/para_details-dark", methods=["GET", "POST"])
-def detparadark():
-    if request.method == "POST":
-        var = request.form.get("subject")
-        gr = grade(var)
-        return render_template(grade_main_dark, grade=gr)
+# @app.route("/para_details", methods=["GET", "POST"])
+# def detpara():
+#     if request.method == "POST":
+#         var = request.form.get("subject")
+#         gr = grade(var)
+#         return render_template(grade_main, grade=gr)
 
 
-@app.route("/contact")
-def cont_us():
-    return render_template(contact_us)
+# @app.route("/para_details-dark", methods=["GET", "POST"])
+# def detparadark():
+#     if request.method == "POST":
+#         var = request.form.get("subject")
+#         gr = grade(var)
+#         return render_template(grade_main_dark, grade=gr)
 
 
-@app.route("/contact-dark")
-def cont_us_dark():
-    return render_template(contact_us_dark)
+# @app.route("/contact")
+# def cont_us():
+#     return render_template(contact_us)
 
 
-@app.route("/contact_details", methods=["GET", "POST"])
-def condet():
-    if request.method == "POST":
-        mes = request.form.get("message")
-        nm = request.form.get("name")
-        f = open("messages.txt", "a")
-        content = "Name: "+nm+"  Message:"+mes+"\n"
-        f.write(content)
-        f.close()
-    return render_template(fin)
+# @app.route("/contact-dark")
+# def cont_us_dark():
+#     return render_template(contact_us_dark)
 
 
-@app.route("/contact_details_dark", methods=["GET", "POST"])
-def condetdark():
-    if request.method == "POST":
-        mes = request.form.get("message")
-        nm = request.form.get("name")
-        f = open("messages.txt", "a")
-        content = "Name: "+nm+"  Message:"+mes+"\n"
-        f.write(content)
-        f.close()
-    return render_template(findr)
-
-# ENKRYPT
+# @app.route("/contact_details", methods=["GET", "POST"])
+# def condet():
+#     if request.method == "POST":
+#         mes = request.form.get("message")
+#         nm = request.form.get("name")
+#         f = open("messages.txt", "a")
+#         content = "Name: "+nm+"  Message:"+mes+"\n"
+#         f.write(content)
+#         f.close()
+#     return render_template(fin)
 
 
-def isstring(string):
-    '''Checks if entry is a valid string'''
-    for i in range(len(string)):
-        if string[i].isalpha():
-            pass
-        else:
-            return False
+# @app.route("/contact_details_dark", methods=["GET", "POST"])
+# def condetdark():
+#     if request.method == "POST":
+#         mes = request.form.get("message")
+#         nm = request.form.get("name")
+#         f = open("messages.txt", "a")
+#         content = "Name: "+nm+"  Message:"+mes+"\n"
+#         f.write(content)
+#         f.close()
+#     return render_template(findr)
+
+# # ENKRYPT
 
 
-def encrypt(text, key="ZXCVMNBLKJFGHDSAQWEYTRUIOP"):
-    '''Encrypts the normal text as per the key'''
-    if len(key) != 26 or key.isupper() == False or isstring(text) == False:
-        return "Invalid Text, please remove numbers,symbols or spaces"
-    else:
-        normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        cipher_text = ""
-        key_list = list(key)
-        normal_list = list(normal)
-        new_plain = text.upper()
-
-        for i in range(len(new_plain)):
-            if new_plain[i].isalpha():
-                index = normal_list.index(new_plain[i])
-                cipher_text = cipher_text + key_list[index]
-            else:
-                cipher_text = cipher_text + new_plain[i]
-        return cipher_text
+# def isstring(string):
+#     '''Checks if entry is a valid string'''
+#     for i in range(len(string)):
+#         if string[i].isalpha():
+#             pass
+#         else:
+#             return False
 
 
-def decrypt(text, key="ZXCVMNBLKJFGHDSAQWEYTRUIOP"):
-    '''Decrypts the encrypted text as per the key'''
+# def encrypt(text, key="ZXCVMNBLKJFGHDSAQWEYTRUIOP"):
+#     '''Encrypts the normal text as per the key'''
+#     if len(key) != 26 or key.isupper() == False or isstring(text) == False:
+#         return "Invalid Text, please remove numbers,symbols or spaces"
+#     else:
+#         normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#         cipher_text = ""
+#         key_list = list(key)
+#         normal_list = list(normal)
+#         new_plain = text.upper()
 
-    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    plain_text = ""
-    key_list = list(key)
-    normal_list = list(normal)
-    new_plain = text.upper()
-    if len(key) != 26 or key.isupper() == False or isstring(text) == False:
-        print("KeyError: Key is not valid")
-        return
-    else:
-        for i in range(len(text)):
-            if new_plain[i].isalpha():
-                index = key_list.index(new_plain[i])
-                plain_text = plain_text + normal_list[index]
-            else:
-                plain_text = plain_text + new_plain[i]
-        return plain_text
-
-
-@app.route("/enkrypt")
-def homeencry():
-    global logged
-    if logged == True:
-        return render_template("en-home.html")
-    else:
-        return render_template("newlogin.html", error="Please login to continue")
+#         for i in range(len(new_plain)):
+#             if new_plain[i].isalpha():
+#                 index = normal_list.index(new_plain[i])
+#                 cipher_text = cipher_text + key_list[index]
+#             else:
+#                 cipher_text = cipher_text + new_plain[i]
+#         return cipher_text
 
 
-@app.route("/enkrypt_details", methods=["GET", "POST"])
-def detencry():
-    if request.method == "POST":
-        text = request.form.get("text")
-        radio = request.form.get("gridRadios")
-        print(text, radio)
-        if radio == "encrypt":
-            fin = encrypt(text)
-            return render_template("en-result.html", result=fin)
-        else:
-            fin = decrypt(text)
-            return render_template("en-result.html", result=fin)
-    return render_template("en-home.html")
+# def decrypt(text, key="ZXCVMNBLKJFGHDSAQWEYTRUIOP"):
+#     '''Decrypts the encrypted text as per the key'''
+
+#     normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#     plain_text = ""
+#     key_list = list(key)
+#     normal_list = list(normal)
+#     new_plain = text.upper()
+#     if len(key) != 26 or key.isupper() == False or isstring(text) == False:
+#         print("KeyError: Key is not valid")
+#         return
+#     else:
+#         for i in range(len(text)):
+#             if new_plain[i].isalpha():
+#                 index = key_list.index(new_plain[i])
+#                 plain_text = plain_text + normal_list[index]
+#             else:
+#                 plain_text = plain_text + new_plain[i]
+#         return plain_text
 
 
-# TODOList
+# @app.route("/enkrypt")
+# def homeencry():
+#     global logged
+#     if logged == True:
+#         return render_template("en-home.html")
+#     else:
+#         return render_template("newlogin.html", error="Please login to continue")
 
-@app.route('/todo')
-def todocon():
-    return render_template("todolist.html")
+
+# @app.route("/enkrypt_details", methods=["GET", "POST"])
+# def detencry():
+#     if request.method == "POST":
+#         text = request.form.get("text")
+#         radio = request.form.get("gridRadios")
+#         print(text, radio)
+#         if radio == "encrypt":
+#             fin = encrypt(text)
+#             return render_template("en-result.html", result=fin)
+#         else:
+#             fin = decrypt(text)
+#             return render_template("en-result.html", result=fin)
+#     return render_template("en-home.html")
+
+
+# # TODOList
+
+# @app.route('/todo')
+# def todocon():
+#     return render_template("todolist.html")
 
 
 # Debugging Statements
